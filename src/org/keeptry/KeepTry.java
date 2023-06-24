ef091c0b224078210b364d5bb53ed5275811e079
Sivasankar93 <sivasankar2658@gmail.com>
2023-06-25 00:14:17 +0530

    Push for B

--- src/org/keeptry/KeepTry.java
+++ src/org/keeptry/KeepTry.java
@@ -3,8 +3,20 @@ package org.keeptry;
 public class KeepTry {
 
 	public static void main(String[] args) {
-		String s= "Sankar";
-		System.out.println(s);
-
+		String s= "sankar";
+		String rev="";
+		System.out.println(s.length());
+		
+	for (int i=s.length()-1;i>=0; i--) {
+		char charAt = s.charAt(i);
+		rev=rev+charAt;	
+	}
+	System.out.println(rev);
+if(s.equalsIgnoreCase(rev)) {
+	System.out.println("Palindrome");
+}
+else {
+	System.out.println("Not Palindrome");
+}
 }
 }
