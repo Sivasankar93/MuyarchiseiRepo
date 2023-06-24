59caefa1b26ff174cde8b1566259a70c742baeb0
Sivasankar93 <sivasankar2658@gmail.com>
2023-06-24 23:41:50 +0530

    Push for A

--- src/org/keeptry/KeepTry.java
+++ src/org/keeptry/KeepTry.java
@@ -3,8 +3,20 @@ package org.keeptry;
 public class KeepTry {
 
 	public static void main(String[] args) {
-		String s= "Sankar";
-		System.out.println(s);
-
+		String s= "MADAM";
+	System.out.println(s);
+	
+		StringBuilder strBuilder= new StringBuilder(s);
+		StringBuilder reverse = strBuilder.reverse();
+		String r = reverse.toString();
+		System.out.println(r);
+		
+if (s.equals(r)) {
+	System.out.println("Palindrome");
 }
+else {
+	System.out.println("Not palindrome");
+}
+	}
+	
 }
